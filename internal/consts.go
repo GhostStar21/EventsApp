60307c84b85aa14eba644457f422d1ac0528deb8
GhostStar21 <ssitdm3@gmail.com>
2026-06-15 13:42:06 +0200

    refactor: reorganize project folder structure

--- internal/consts.go
+++
@@ -1,6 +0,0 @@
-package internal
-
-const EventsPath = "/v1/events/"
-const 
-
-
---
+++ internal/consts/consts.go
@@ -0,0 +1,7 @@
+package consts
+
+const EventsPath = "/v1/events/"
+const OrganizersPath = "/v1/organizers/"
+const UsersPath = "/v1/users/"
+
+
