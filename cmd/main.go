57970fc57fced0c64177374bef542e7b4b0d251e
GhostStar21 <ssitdm3@gmail.com>
2026-06-18 14:21:49 +0200

    chore(main): connect to DB schema

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,21 @@ import (
 	"EventsApp/internal/consts"
 	"EventsApp/internal/events"
 	"EventsApp/internal/organizers"
+	"EventsApp/internal/postgres"
 	"EventsApp/internal/users"
+	"context"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Printf("No .env file loaded: %v", err)
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Println("$PORT has not been set. Default: 8080")
@@ -18,6 +26,21 @@ func main() {
 	} else {
 		log.Println("$PORT has been set: ", port)
 	}
+
+	ctx := context.Background()
+	db, err := postgres.NewPool(ctx)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+
+	// Provide DB pool to packages that need it
+	events.SetDB(db)
+	users.SetDB(db)
+	organizers.SetDB(db)
+
+	defer db.Close()
+
+	log.Println("Successfully connected to database")
 
 	router := http.NewServeMux()
 
@@ -29,5 +52,4 @@ func main() {
 	if err := http.ListenAndServe(":"+port, router); err != nil {
 		log.Printf("Failed to start server: %v", err)
 	}
-
 }
