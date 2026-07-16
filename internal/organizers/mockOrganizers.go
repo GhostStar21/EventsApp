2e93aac007beae1536eaf99cb42835129716d39b
GhostStar21 <ssitdm3@gmail.com>
2026-07-16 19:39:54 +0200

    refactor: removed duplicates after earlier refactor of directory

--- internal/organizers/mockOrganizers.go
+++
@@ -1,8 +0,0 @@
-package organizers
-
-var organizers = []Organizer{
-	{Id: 1, Name: "Acme Events", OrgNumber: 101},
-	{Id: 2, Name: "GoConf", OrgNumber: 102},
-	{Id: 3, Name: "Darling", OrgNumber: 103},
-	{Id: 4, Name: "Login", OrgNumber: 104},
-}
--- internal/organizers/organizersStruct.go
+++
@@ -1,7 +0,0 @@
-package organizers
-
-type Organizer struct {
-	Id        int    `json:"id"`
-	Name      string `json:"name"`
-	OrgNumber int    `json:"orgnumber"`
-}
